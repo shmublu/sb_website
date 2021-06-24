6c64eb746647005d2498e832189bd7be81b40b6f
shmublu <samuelalexanderberman@gmail.com>
2021-06-24 17:27:22 -0400

    initial files

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const tbar = props => (
+    <header>
+        <nav>
+            <div class ="my-name">Shmuel Berman</div>
+            <div class="toolbar-links">
+                <ul>
+                    <li><a href="/">CV</a></li>
+                    <li><a href="/">Publications</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default tbar;
